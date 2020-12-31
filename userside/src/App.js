93670bfacea2f8956e5beabb8fde25ec0b4ee104
Sanganu <kppkmsanu@gmail.com>
2020-12-31 00:31:39 -0700

    materialize react

--- userside/src/App.js
+++ userside/src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import 'materialize-css';
-import './App.css';
-import { Parallax } from "react-materialize";
+import 'materialize-css/dist/css/materialize.min.css';
+// import 'materialize-css';
+// import './App.css';
+// import { Parallax } from "react-materialize";
 import Homepage from "./pages/Homepage";
 import Navigation from "./pages/Navigation";
 
